9552993c29edd0796abad1619e32bf308eff8b80
n-satoshi061 <n.satoshi061@gmail.com>
2020-06-22 19:33:24 +0900

    削除機能実装(途中)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.user_id == current_user.id
+      item.destroy
+      redirect_to("/")
+    end
+  end
 end
