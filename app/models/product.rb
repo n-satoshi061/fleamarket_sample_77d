3d43afb64e4f3fb1ad128b4861039eb2b569166a
n-satoshi061 <n.satoshi061@gmail.com>
2020-06-25 10:15:19 +0900

    DB設計

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
   has_many :images
-  belongs_to :user
+  belongs_to :buyer, class_name: 'User', :foreign_key => 'buyer_id', optional: true
+  belongs_to :seller, class_name: 'User', :foreign_key => 'seller_id'
   belongs_to :category
+  accepts_nested_attributes_for :images, allow_destroy: true
 end
