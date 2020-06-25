b14760fcd4e62db2ecfc7b7960420e303995e0f5
kerry2259 <65342456+kerry2259@users.noreply.github.com>
2020-06-26 00:50:31 +0900

    【マークアップ】商品詳細ページコードレビュー

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
   def index
   end
 
+  def show
+  end
+  
   def destroy
     item = Item.find(params[:id])
     if item.user_id == current_user.id
