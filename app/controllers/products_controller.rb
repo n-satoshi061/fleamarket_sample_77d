10d74c81caa88a5ab279a40d7f8bb10396762011
n-satoshi061 <n.satoshi061@gmail.com>
2020-06-29 09:18:05 +0900

    カテゴリー機能(途中)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,33 @@
 class ProductsController < ApplicationController
   before_action :move_to_index, except: :index
+  before_action :set_item, only:[:show, :destroy, :edit, :update, :purchase, :payment]
 
   def index
     @product = Product.all
+  end
+
+  def new
+    @item = Item.new
+    @item.item_images.new
+
+    #セレクトボックスの初期値設定
+    @category_parent_array = ["---"]
+    #データベースから、親カテゴリーのみ抽出し、配列化
+    @category_parent_array = Category.where(ancestry: nil)
+  end
+
+  # 以下全て、formatはjsonのみ
+  # 親カテゴリーが選択された後に動くアクション
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    # ここでfind_byを使うことでレディーしか取れてなかった
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+  # 子カテゴリーが選択された後に動くアクション
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
   def show
@@ -12,9 +37,9 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.user_id == current_user.id
-      item.destroy
+    product = Product.find(params[:id])
+    if product.user_id == current_user.id
+      product.destroy
       redirect_to("/")
     end
   end
