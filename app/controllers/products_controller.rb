e093d64ec1dab1fe3990bd576fe9d8cd430d3b50
7oops4 <65325376+7oops4@users.noreply.github.com>
2020-07-01 17:10:45 +0900

    Merge branch 'master' into 【サーバサイド】商品購入機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,25 @@
 class ProductsController < ApplicationController
-  before_action :move_to_index, except: :index
 
   def index
     @product = Product.all
   end
 
+  def new
+    @product = Product.new
+    @product.images.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+    redirect_to root_path
+    else
+    render :new
+    end
+  end
+
   def show
+    @product = Product.find(params[:id])
   end
 
   def buy
@@ -23,4 +37,13 @@ class ProductsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
+
+  def product_params
+    params.require(:product).permit(:name, :price, images_attributes: [:src, :_destroy, :id])
+  end
+  
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, only: [:index, :show, :buy, :destroy]
+  resources :products, only: [:index, :new, :show, :buy, :destroy]
   resources :signup, only: :create do
     collection do
       get 'step1'
       get 'step2'
     end
   end
-  resources :users, only: [:index]
+  resources :users, only: [:index] do
+    collection do
+      get 'logout_page'
+    end
+  end
   resources :cards, only: [:new]
 end
